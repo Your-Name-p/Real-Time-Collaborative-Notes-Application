3a29d8c91d1f98a163b340fe84ec7a4960015bcb
Arjun <arjunpsingh111@gmail.com>
2026-01-21 14:30:02 +0530

    Fix Netlify deployment issues

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3333',
 });
 
 api.interceptors.request.use((config) => {
@@ -11,5 +11,13 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('API Error:', error.response?.data || error.message);
+    return Promise.reject(error);
+  }
+);
 
 export default api;
